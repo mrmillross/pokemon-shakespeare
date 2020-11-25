f253383fca08708e918ee59f6c203e021b59041b
Matt Millross <millross@gmail.com>
2020-11-25 18:04:56 +0000

    add gitignore and basic stuff to startup that this shall need.

--- PokemonShakespeareApi/Startup.cs
+++ PokemonShakespeareApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PokemonShakespeareApi.Controllers;
 
 namespace PokemonShakespeareApi
 {
@@ -25,6 +26,9 @@ namespace PokemonShakespeareApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<PokemonConfig>(Configuration.GetSection("PokemonConfig"));
+            services.Configure<ShakespeareConfig>(Configuration.GetSection("ShakespeareConfig"));
+            services.AddHttpClient();
             services.AddControllers();
         }
 
